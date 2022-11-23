95dbb920e350a436ff8ddce7be72eddb88092000
Tomasz Broniek <tbrony@gmail.com>
2022-11-23 16:03:04 +0100

    New changes

--- PocFlexibleETL/LoadRawData.py
+++ PocFlexibleETL/LoadRawData.py
@@ -11,8 +11,12 @@ StationsJSON = (GetDataFromApi(AllStatURL))
 
 FromRawDataToStgTable('RestApi',StationsJSON)
 StationsFromRestApi = pd.json_normalize(StationsJSON,max_level=2)
-print(StationsFromRestApi)
-print(StationsFromRestApi.loc[:,["id","stationName","city.id","city.name"]])
+RAPI_Table_Stations = StationsFromRestApi.loc[:,["id","stationName","gegrLat","gegrLon","addressStreet","city.id","city.id","city.name","city.commune.communeName","city.commune.districtName","city.commune.provinceName"]].set_index('id')
+DB_Table_Stations = TableFromDB('dbo.AllStations',ConString).set_index('id')
+RAPI_Table_StationsCities = pd.merge(RAPI_Table_Stations,DB_Table_Stations, how='left',on=['id'],suffixes=('_left','_right')).query('stationName_right.isnull()')
+print(RAPI_Table_StationsCities.head())
+#print(RAPI_Table_StationsCities)
+#DimenstionStations = pd.merRAPI_Table_Stations
 #DimensionStations = StationsFromRestApi.loc[:,["id","stationName","city","addressStreet"]]
 # StationsIds = StationsFromRestApi.loc[:,["id"]]
 # All = (GiveMeAllAvaiableSensor(StationsIds))
